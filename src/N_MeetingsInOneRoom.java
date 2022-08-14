2d5b7e5609fa225de7eb6c3742ee535e9797b2bf
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-13 22:20:29 -0700

    Initial commit

---
+++ src/N_MeetingsInOneRoom.java
@@ -0,0 +1,20 @@
+class Pair{
+    int start;
+    int end;
+    Pair(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+}
+
+
+public class N_MeetingsInOneRoom {
+    static int n_MeetingsInOneRoom(int []start, int [] end, int n){
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
