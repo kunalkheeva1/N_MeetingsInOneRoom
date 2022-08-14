439c118b26f9a4d4a62c7fa636faa6ef9ef53a6d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-13 22:32:31 -0700

    coded a method return the count of the total meetings that can take place in one room with respect to the starting and ending times of others

--- src/N_MeetingsInOneRoom.java
+++ src/N_MeetingsInOneRoom.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 class Pair{
     int start;
     int end;
@@ -10,6 +13,22 @@ class Pair{
 
 public class N_MeetingsInOneRoom {
     static int n_MeetingsInOneRoom(int []start, int [] end, int n){
+
+        Pair []arr = new Pair[n];
+        for(int i=0;i<n; i++){
+            Pair p = new Pair(start[i],end[i]);
+            arr[i]= p;
+        }
+        Arrays.sort(arr,(p1,p2)-> p1.end-p2.end);
+        int count =1;
+        int current = arr[0].end;
+        for(int i=1; i<n; i++){
+            if(arr[i].start >current){
+                count ++;
+                current = arr[i].end;
+            }
+        }return count;
+
 
     }
 
